2015d088512db8fc946e0850d467657d59c22c6e
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-07-31 23:46:56 +0500

    new

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+// description: Auth user/set token
+// route POST/api/users/auth
+// access: public
+
+const authUser = (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+};
+
+export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
-const PORT = 5000;
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("Server is Ready!"));
 
