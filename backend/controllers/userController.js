f441f62e2c1ce245059a5cb0b14f780901aa7d68
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-08-02 23:45:12 +0500

    user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,21 @@ import generateToken from "./../utils/generateTokens.js";
 // access: public
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id_,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password.");
+  }
 });
 
 // description: Register a new user
