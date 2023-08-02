2b11c6636e5e46d16b281ce5f06775086e1e4d82
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-08-02 23:25:04 +0500

    worked on jwt tokenization

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
-// import User from "./../models/userModel.js";
+import User from "../models/userModel.js";
+import generateToken from "./../utils/generateTokens.js";
 
 // description: Auth user/set token
 // route POST/api/users/auth
@@ -14,8 +15,32 @@ const authUser = asyncHandler(async (req, res) => {
 // access: public
 
 const registerUser = asyncHandler(async (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id_,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data.");
+  }
 });
 
 // description: Logout user
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = mongoose.SchemaType(
+const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
@@ -20,6 +21,17 @@ const userSchema = mongoose.SchemaType(
     timestamps: true,
   }
 );
+
+//  middleware to hash password
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
