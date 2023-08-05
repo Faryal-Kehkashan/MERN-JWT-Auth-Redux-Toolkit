3d6053c096d4f246e425997240e20b8c43b9a663
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-08-05 19:14:14 +0500

    lougout functionality & loader component

--- frontend/src/slices/userApiSlice.js
+++ frontend/src/slices/userApiSlice.js
@@ -10,7 +10,13 @@ export const userApiSlice = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USER_URL}/logout`,
+        method: "POST",
+      }),
+    }),
   }),
 });
 
-export const { useLoginMutation } = userApiSlice;
+export const { useLoginMutation, useLogoutMutation } = userApiSlice;
