66d5fded389fa5a3d533b953a1cd1d1135ffc694
Faryalkehkashan <faryalkehkashanfoa@gmail.com>
2023-08-06 17:19:11 +0500

    worked on production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
@@ -20,7 +21,16 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("Server is Ready!"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is Ready!"));
+}
 
 app.use(notFound);
 app.use(errorHander);
